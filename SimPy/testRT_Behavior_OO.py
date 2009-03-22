6a6c78f44bc4753b9b79e452cbd5f7ef3b3e55a2
kgmuller <devnull@localhost>
2009-03-22 21:47:50 +0000

    Change of output.

--- SimPy/testRT_Behavior_OO.py
+++ SimPy/testRT_Behavior_OO.py
@@ -3,10 +3,12 @@ from SimPy.SimulationRT import *
 """testRT_Behavior_OO.py
 Tests SimulationRT for degree to which simulation time
 and wallclock time can be synchronized.
+
+OO API.
 """
 # $Revision: $ $Date:  $
 
-print "Under test: SimulationRT.py %s"%simulationVersion
+print "Under test: SimulationRT.py"
 __version__ = '2.0 $Revision:  $ $Date: $ '
 print 'testRT_Behavior.py %s'%__version__
 
@@ -25,8 +27,10 @@ t=Ticker(sim=s)
 s.activate(t,t.tick())
 s.simulate(until=10,real_time=True,rel_speed=1)
 
-for obs in t.timing:
-    print obs,obs[0]/obs[1]
+print "Speed ratio set: 1"
+print "------------------"
+for tSim,tRT in t.timing:
+    print "tSim:%s, tRT:%s, speed ratio:%s"%(tSim,tRT,tSim/tRT)
       
 s1=SimulationRT()
 s1.initialize()
@@ -34,5 +38,8 @@ t=Ticker(sim=s1)
 s1.activate(t,t.tick())
 s1.simulate(until=10,real_time=True,rel_speed=5)
 
-for obs in t.timing:
-    print obs,obs[0]/obs[1]
+print
+print "Speed ratio set: 5"
+print "------------------"
+for tSim,tRT in t.timing:
+    print "tSim:%s, tRT:%s, speed ratio:%s"%(tSim,tRT,tSim/tRT)
