2d122140f3ea75fae4af1eab56ead759b4dec98b
Ontje L?nsdorf <the_com@gmx.de>
2011-08-17 20:17:43 +0200

    Added global tests for step and trace.

--- SimPy/test/support.py
+++ SimPy/test/support.py
@@ -1,29 +1,37 @@
 # coding=utf-8
 
 from SimPy import Globals, Simulation, SimulationStep, SimulationTrace
-from SimPy import Globals
 
 
 def pytest_generate_tests(metafunc):
     if 'sim' in metafunc.funcargnames:
-        # Execute tests using the global simulation object (SimPy 1.x style).
-        metafunc.addcall(param='global')
         # Execute tests using a dedicated simulation instance (SimPy OO style).
-        metafunc.addcall(param='oo')
+        metafunc.addcall(param='default')
         # Execute tests using a SimulationStep instance.
         metafunc.addcall(param='step')
         # Execute tests using a SimulationTrace instance.
         metafunc.addcall(param='trace')
 
+        # Execute tests using the global simulation object (SimPy 1.x style).
+        metafunc.addcall(param='global-default')
+        # Execute tests using the global SimulationStep object.
+        metafunc.addcall(param='global-step')
+        # Execute tests using the global SimulationTrace object.
+        metafunc.addcall(param='global-trace')
+
 
 def pytest_funcarg__sim(request):
-    if request.param == 'global':
-        # Clear global simulation so that it can be used in multiple tests.
-        Globals.sim.initialize()
-        return Globals.sim
-    elif request.param == 'oo':
+    if request.param == 'default':
         return Simulation.Simulation()
     elif request.param == 'step':
         return SimulationStep.SimulationStep()
     elif request.param == 'trace':
         return SimulationTrace.SimulationTrace()
+    elif request.param.startswith('global'):
+        if request.param.endswith('default'):
+            Globals.sim = Simulation.Simulation()
+        elif request.param.endswith('step'):
+            Globals.sim = SimulationStep.Simulation()
+        elif request.param.endswith('trace'):
+            Globals.sim = SimulationTrace.Simulation()
+        return Globals.sim
