6c5f76a1a1c29f007218140c2923a7dcf7c3fc40
Ontje L?nsdorf <the_com@gmx.de>
2011-09-16 20:25:08 +0200

    Beautified test output.

--- docs/source/Manuals/programs/conftest.py
+++ docs/source/Manuals/programs/conftest.py
@@ -4,6 +4,9 @@ import os.path
 import subprocess
 
 import pytest
+from py._code.code import TerminalRepr, ReprFileLocation
+from _pytest.assertion.util import _diff_text
+
 
 def pytest_collect_file(path, parent):
     if path.ext != '.py': return
@@ -22,6 +25,51 @@ class TutorialItem(pytest.Item):
         root, ext = os.path.splitext(str(self.fspath))
         with open(root + '.OK') as f:
             expected = f.read()
-        output = subprocess.check_output(['python', str(self.fspath)])
-        assert output == expected
+        output = subprocess.check_output(['python', str(self.fspath)],
+                stderr=subprocess.STDOUT)
+        if output != expected:
+            raise ValueError(expected, output)
 
+    def repr_failure(self, exc_info):
+        if exc_info.errisinstance((ValueError,)):
+            expected, output = exc_info.value.args
+            message = _diff_text(expected, output)
+            return ReprFailTutorial(self.fspath.basename, message)
+        elif exc_info.errisinstance((subprocess.CalledProcessError,)):
+            return ReprErrorTutorial(self.fspath.basename, exc_info)
+        else:
+            return pytest.Item.repr_failure(self, exc_info)
+
+    def reportinfo(self):
+        return self.fspath, None, '[tutorial %s]' % self.fspath.basename
+
+
+class ReprFailTutorial(TerminalRepr):
+    Markup = {
+            '+': dict(green=True),
+            '-': dict(red=True),
+            '?': dict(bold=True),
+    }
+
+    def __init__(self, filename, message):
+        self.filename = filename
+        self.message = message
+
+    def toterminal(self, tw):
+        for line in self.message:
+            markup = ReprFailTutorial.Markup.get(line[0], {})
+            tw.line(line, **markup)
+        tw.line('%s: Unexpected output' % self.filename)
+
+
+class ReprErrorTutorial(TerminalRepr):
+    def __init__(self, filename, exc_info):
+        self.filename = filename
+        self.exc_info = exc_info
+
+    def toterminal(self, tw):
+        tw.line('Execution failed! Captured output:', bold=True)
+        tw.sep('-')
+        tw.line(self.exc_info.value.output, red=True, bold=True)
+        tw.line('%s: Tutorial failed (exitcode=%d)' % (self.filename,
+                self.exc_info.value.returncode))
