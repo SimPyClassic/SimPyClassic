22fb8a992e723b14f06fc33ad929f8b5936fdbdd
Stefan Scherfke <stefan@sofa-rockers.org>
2011-11-30 10:38:47 +0100

    Fixed (and flake8?ed) examples to run under Python 3.

--- docs/source/Manuals/programs/diffpriority.py
+++ docs/source/Manuals/programs/diffpriority.py
@@ -1,22 +1,25 @@
-from SimPy.Simulation import *
+from SimPy.Simulation import (PriorityQ, Process, Resource, activate,
+        initialize, hold, now, release, request, simulate)
+
 
 class Client(Process):
-   def __init__(self,name):
-       Process.__init__(self,name)
+    def __init__(self, name):
+        Process.__init__(self, name)
 
-   def getserved(self,servtime,priority,myServer):
-       print self.name, 'requests 1 unit at t=',now()
-       yield request, self, myServer, priority
-       yield hold, self, servtime
-       yield release, self,myServer
-       print self.name,'done at t= ',now()
+    def getserved(self, servtime, priority, myServer):
+        print('%s requests 1 unit at t=%s' % (self.name, now()))
+        yield request, self, myServer, priority
+        yield hold, self, servtime
+        yield release, self, myServer
+        print('%s done at t=%s' % (self.name, now()))
+
 
 initialize()
 # create the *server* Resource object
-server=Resource(capacity=1,qType=PriorityQ,preemptable=1)
+server = Resource(capacity=1, qType=PriorityQ, preemptable=1)
 # create some Client process objects
-c1=Client(name='c1')
-c2=Client(name='c2')
-activate(c1,c1.getserved(servtime=100,priority=1,myServer=server),at=0)
-activate(c2,c2.getserved(servtime=100,priority=9,myServer=server),at=50)
+c1 = Client(name='c1')
+c2 = Client(name='c2')
+activate(c1, c1.getserved(servtime=100, priority=1, myServer=server), at=0)
+activate(c2, c2.getserved(servtime=100, priority=9, myServer=server), at=50)
 simulate(until=500)
--- docs/source/Manuals/programs/message.py
+++ docs/source/Manuals/programs/message.py
@@ -1,22 +1,23 @@
-from SimPy.Simulation import *
+from SimPy.Simulation import Process, activate, initialize, hold, now, simulate
+
 
 class Message(Process):
- """ a simple Process """
- def __init__(self,i,len):
-     Process.__init__(self,name="Message"+str(i))
-     self.i = i
-     self.len = len
+    """A simple Process"""
+    def __init__(self, i, len):
+        Process.__init__(self, name='Message' + str(i))
+        self.i = i
+        self.len = len
 
- def go(self):
-     print now( ), self.i, "Starting"
-     yield hold,self,100.0
-     print now( ), self.i, "Arrived"
+    def go(self):
+        print('%s %s %s' % (now(), self.i, 'Starting'))
+        yield hold, self, 100.0
+        print('%s %s %s' % (now(), self.i, 'Arrived'))
 
-initialize( )
-p1  = Message(1,203)  # new message
-activate(p1,p1.go( )) # activate it
-p2  = Message(2,33)
-activate(p2,p2.go( ),at=6.0)
+
+initialize()
+p1 = Message(1, 203)   # new message
+activate(p1, p1.go())  # activate it
+p2 = Message(2, 33)
+activate(p2, p2.go(), at=6.0)
 simulate(until=200)
-print 'Current time is ',now( ) # will print 106.0
-
+print('Current time is %s' % now())  # will print 106.0
